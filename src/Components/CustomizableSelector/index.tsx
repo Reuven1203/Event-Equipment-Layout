8ce4083e6a6b0eafa025d757773eb462ca6b18a1
Reuven1203 <reuven1203@gmail.com>
2023-10-14 17:31:20 -0400

    Customize form

---
+++ src/Components/CustomizableSelector/index.tsx
@@ -0,0 +1,178 @@
+import {FC, useEffect,useReducer} from 'react';
+import {FormControlLabel,Checkbox, Rating} from '@mui/material';
+import {Speaker,SpeakerGroup, Light, Fluorescent} from '@mui/icons-material';
+import Card from '../PackageSelector/Card';
+
+interface EquipmentState {
+    checked: boolean;
+    quantity: number;
+}
+
+type State = {
+    [equipmentName: string]: EquipmentState;
+};
+
+// Action types
+interface ToggleCheckAction {
+    type: 'TOGGLE_CHECK';
+    payload: {
+        id: number;
+    };
+}
+
+interface SetQuantityAction {
+    type: 'SET_QUANTITY';
+    payload: {
+        id: number;
+        quantity: number | null;
+    };
+}
+
+
+type Action = ToggleCheckAction | SetQuantityAction;
+
+const initialState = {
+    0: {
+        checked: true,
+        quantity: 1,
+    },
+    1: {
+        checked: false,
+        quantity: 0,
+    },
+    2: {
+        checked: false,
+        quantity: 0,
+    },
+    3: {
+        checked: false,
+        quantity: 0,
+    }
+};
+
+const reducer = (state: State, action: Action) => {
+    switch (action.type) {
+        case 'TOGGLE_CHECK':
+            return {
+                ...state,
+                [action.payload.id]: {
+                    ...state[action.payload.id],
+                    checked: !state[action.payload.id].checked,
+                    quantity:1
+                },
+            };
+        case 'SET_QUANTITY':
+            return {
+                ...state,
+                [action.payload.id]: {
+                    ...state[action.payload.id],
+                    checked: true,
+                    quantity: action.payload.quantity ?? 0,
+                },
+            };
+        default:
+            return state;
+    }
+};
+
+const CustomizableSelector:FC<{isSelected:boolean, onClick: (index:number)=>void}> = (props) => {
+    const screenW = window.innerWidth;
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        console.log("Current form state:", state);
+    }, [state]);
+
+    const equipmentList = [{
+        id: 0,
+        name: 'Speaker',
+        icon: <SpeakerGroup sx={{color:'white'}}/>,
+        emptyIcon: <SpeakerGroup/>,
+        min: 1,
+        max:3,
+    },
+        {
+            id: 1,
+            name: 'Bass',
+            icon: <SpeakerGroup sx={{color:'white'}}/>,
+            emptyIcon: <SpeakerGroup/>,
+            min: 0,
+            max: state[0].quantity
+
+        },
+        {
+            id: 2,
+            name: 'Moving Head',
+            icon: <Light sx={{color:'white'}}/>,
+            emptyIcon: <Light/>,
+            min: 0,
+            max: 4
+
+
+        },
+        {
+            id: 3,
+            name: 'Gigbar',
+            icon: <Fluorescent sx={{color:'white'}}/>,
+            emptyIcon: <Fluorescent/>,
+
+        }
+    ];
+
+    const handleCheckboxChange = (equipmentId: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: 'TOGGLE_CHECK',
+            payload: {
+                id: equipmentId
+            },
+        });
+    };
+
+    const handleRatingChange = (equipmentId: number) => (event: any, newValue: number | null) => {
+        let finalValue = newValue;
+
+        if (equipmentId === 0 && (!newValue || newValue < 1)) {  // Assuming the Speaker has id 0
+            finalValue = 1;
+        }
+
+        dispatch({
+            type: 'SET_QUANTITY',
+            payload: {
+                id: equipmentId,
+                quantity: finalValue,
+            },
+        });
+    };
+
+    const onClickHandler = () => {
+        if (screenW > 640) {
+            props.onClick(100);
+        }
+    }
+
+    return (
+        <Card isSelected={props.isSelected} onClick={onClickHandler} isCustomize={true}>
+            <h2 className={'w-full text-center align-middle'}>Customize</h2>
+            <div className={'p-3 flex-col overflow-scroll'}>
+                {equipmentList.map((equipment) => {
+                        return (
+                            <div key={equipment.id} className={'flex items-center'}>
+                                <FormControlLabel  control={<Checkbox  checked={state[equipment.id]?.checked} onChange={equipment.id == 0 ? undefined :handleCheckboxChange(equipment.id)} sx={{
+                                    color: 'white',
+                                    '&.Mui-checked': {
+                                        color: 'white',
+                                    },
+                                }}/>} name={equipment.name} id={equipment.name} label={equipment.name} />
+                                <div>
+                                    <Rating value={state[equipment.id].checked? state[equipment.id].quantity : 0} onChange={handleRatingChange(equipment.id)} name={equipment.name} max={equipment.max} icon={equipment.icon} emptyIcon={equipment.emptyIcon}   />
+                                </div>
+                            </div>
+                        )
+                    }
+                )}
+            </div>
+        </Card>
+    );
+};
+
+export default CustomizableSelector;
--- src/Components/PackageSelector/index.tsx
+++ src/Components/PackageSelector/index.tsx
@@ -1,4 +1,5 @@
-import {FC, useState} from 'react';
+import {FC} from 'react';
+import Card from './Card';
 
 interface item {
     name: string;
@@ -16,23 +17,24 @@ const PackageSelector:FC<{index:number,title:string, items?:item[], basePrice:nu
     }
     // const desktopStyle = {`${props.isSelected ? 'md:animate-expand' : 'md:animate-shrink hover:opacity-[0.8]'} h-[350px]  bg-[#2E49A6] text-white  shadow-2xl border-2 border-[#2E49A6] cursor-pointer w-[300px] relative max-md:w-[full]`}
     return (
-        <div onClick={onClickHandler} className={`${props.isSelected ? 'sm:animate-expand' : 'sm:animate-shrink hover:opacity-[0.8]'} sm:h-[350px] h-[300px]  bg-[#2E49A6] text-white  shadow-2xl border-2 border-[#2E49A6] sm:cursor-pointer max-sm:w-[80%] w-[300px] relative `}>
-            <h1 className={'text-[30px] w-full text-center p-2'}>{props.title}</h1>
+        <Card isSelected={props.isSelected} onClick={onClickHandler}>
+            <h2 className={'w-full text-center p-2'}>{props.title}</h2>
             <div>
-                <h2 className={'w-full text-center'}>Estimated Price:</h2>
+                <h3 className={'w-full text-center'}>Estimated Price:</h3>
                 <h1 className={'w-full text-center text-4xl'}> ${equipmentPrice && equipmentPrice + props.basePrice}</h1>
             </div>
-               <ul className="text-[20px] overflow-y-scroll text-center max-h-[40%]">
-                   {props.items?.map((item) => {
-                          return (
-                            <li>{item.quantity}x {item.name} </li>
-                          )
-                   })}
-               </ul>
-               <div className={'absolute bottom-0  w-full p-3'}>
-                   <button className='bg-white text-black rounded-2x w-full'>Get a quote</button>
-               </div>
-       </div>
+            <ul className="text-[20px] overflow-y-scroll text-center max-h-[40%]">
+                {props.items?.map((item) => {
+                    return (
+                        <li>{item.quantity}x {item.name} </li>
+                    )
+                })}
+            </ul>
+            <div className={'absolute bottom-0  w-full p-3'}>
+                <button className='bg-white text-black rounded-2x w-full'>Get a quote</button>
+            </div>
+        </Card>
+
     );
 };
 
